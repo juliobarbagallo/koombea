7a353e9f8416c14a2eec932f14f369854bd94a64
juliobarbagallo <julio.barbagallo@gmail.com>
2023-04-04 10:34:10 -0300

    feat: Adds register and login, forms with templates

--- py_scraper/accounts/views.py
+++ py_scraper/accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.views import LoginView
+from accounts.forms import CustomUserCreationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
